a910a75e438b7a50f1cb25c290d980a78a0ed379
dvegaa20 <diego.vega.camacho@gmail.com>
2025-04-10 22:24:43 -0600

    refactor(visualization): remove commented code

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -34,25 +34,3 @@ def plot_training_history(history):
 
 # test_loss, test_acc = model.evaluate(test_generator)
 # print(f"\nTest accuracy: {test_acc:.4f}")
-
-# # Visualización de métricas
-# acc = history.history["accuracy"]
-# val_acc = history.history["val_accuracy"]
-# loss = history.history["loss"]
-# val_loss = history.history["val_loss"]
-
-# epochs = range(1, EPOCHS + 1)
-
-# plt.figure()
-# plt.plot(epochs, acc, "b", label="Train acc")
-# plt.plot(epochs, val_acc, "r", label="Val acc")
-# plt.title("Train vs Val Accuracy")
-# plt.legend()
-
-# plt.figure()
-# plt.plot(epochs, loss, "b", label="Train loss")
-# plt.plot(epochs, val_loss, "r", label="Val loss")
-# plt.title("Train vs Val Loss")
-# plt.legend()
-
-# plt.show()
