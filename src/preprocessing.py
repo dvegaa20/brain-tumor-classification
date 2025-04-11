3049cfd84aa521bbc17bde2bd7783a155f61b178
dvegaa20 <diego.vega.camacho@gmail.com>
2025-04-10 20:18:23 -0600

    refactor(preprocessing): improve docstring formatting and enhance class name retrieval

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,20 +1,25 @@
 import os
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.utils import shuffle
 
 
 def get_data_info(data_path):
     """
     Prints the number of images in each class in the given directory.
 
-    Args:
-        data_path (str): The path to the directory containing the image data.
+    Parameters
+    ----------
+    data_path (str):
+        The path to the directory containing the image data.
 
-    Returns:
+    Returns
+    -------
         A list of class names.
     """
-    class_names = sorted(os.listdir(data_path))
+    class_names = [
+        cls
+        for cls in os.listdir(data_path)
+        if os.path.isdir(os.path.join(data_path, cls))
+    ]
     class_counts = {
         cls: len(os.listdir(os.path.join(data_path, cls))) for cls in class_names
     }
@@ -23,6 +28,8 @@ def get_data_info(data_path):
     for cls, count in class_counts.items():
         print(f"- {cls}: {count} images")
 
+    print(" ")
+
     return class_names
 
 
@@ -30,10 +37,14 @@ def plot_sample_images(generator, class_names, num_samples=25):
     """
     Plots a specified number of sample images from a data generator.
 
-    Args:
-        generator: A generator of (images, labels) tuples.
-        class_names: A list of class names in the order they are expected in the labels.
-        num_samples (int): The number of sample images to plot (default is 25).
+    Parameters
+    ----------
+    generator:
+        A generator of (images, labels) tuples.
+    class_names:
+        A list of class names in the order they are expected in the labels.
+    num_samples (int):
+        The number of sample images to plot (default is 25).
     """
     images, labels = next(generator)
 
@@ -44,6 +55,6 @@ def plot_sample_images(generator, class_names, num_samples=25):
         plt.yticks([])
         plt.grid(False)
         plt.imshow(images[i].squeeze(), cmap=plt.cm.binary)
-        plt.xlabel(f"{labels[i]} - {class_names[int(labels[i])]}")
+        plt.xlabel(f"{labels[i]} - {class_names[labels[i].argmax()]}")
     plt.tight_layout()
     plt.show()
