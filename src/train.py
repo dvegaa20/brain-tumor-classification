ef2c530581d97924a1e233be350f505c137c91fe
dvegaa20 <diego.vega.camacho@gmail.com>
2025-04-11 00:03:57 -0600

    feat(train): implement training script with different models and data generators

---
+++ src/train.py
@@ -0,0 +1,54 @@
+from keras import optimizers
+from utils.augmentation import create_generators
+from utils.models import (
+    build_basic_cnn,
+    build_enhanced_cnn,
+    build_vgg16,
+    build_mobilenet,
+)
+from keras.callbacks import EarlyStopping
+from utils.visualization import plot_training_history
+
+EPOCHS = 10
+
+# Data Generators
+train_gen, val_gen, test_gen = create_generators(
+    train_dir="data/raw/training",
+    test_dir="data/raw/testing",
+    use_rgb=True,
+)
+
+# Models
+# model = build_basic_cnn()
+# model = build_enhanced_cnn()
+model = build_vgg16()
+model.summary()
+# model = build_mobilenet()
+
+# Callbacks
+callbacks = [
+    EarlyStopping(monitor="val_acc", mode="max", patience=5, restore_best_weights=True),
+]
+
+# Compile the model
+model.compile(
+    loss="binary_crossentropy",
+    optimizer=optimizers.RMSprop(learning_rate=2e-5),
+    metrics=["acc"],
+)
+
+# Train the model
+history = model.fit(
+    train_gen,
+    epochs=EPOCHS,
+    validation_data=val_gen,
+    callbacks=callbacks,
+)
+
+# Save the model
+model.save("models/best_model_vgg16.keras")
+print("Model saved!")
+
+
+# Evaluación final
+plot_training_history(history)
