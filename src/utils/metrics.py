03c6a2650ad7d6e1f43e9f62ee074466d75eda26
dvegaa20 <diego.vega.camacho@gmail.com>
2025-04-22 17:41:59 -0600

    docs(metrics): add docstrings for classification report and confusion matrix functions

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -5,6 +5,24 @@ import numpy as np
 
 
 def generate_classification_report(true_labels, predictions, class_names):
+    """
+    Generates a classification report based on the given true labels and predictions.
+
+    Parameters
+    ----------
+    true_labels : array-like
+        The true labels of the data.
+    predictions : array-like
+        The predictions of the model.
+    class_names : list-like
+        A list of class names in the order they are expected in the labels.
+
+    Returns
+    -------
+    report : str
+        The classification report as a string.
+    """
+    
     report = classification_report(
         true_labels,
         np.argmax(predictions, axis=1),
@@ -15,6 +33,29 @@ def generate_classification_report(true_labels, predictions, class_names):
 
 
 def plot_confusion_matrix(true_labels, predictions, class_names):
+    """
+    Plots a confusion matrix using the given true labels and predictions.
+
+    Parameters
+    ----------
+    true_labels : array-like
+        The true labels of the data.
+    predictions : array-like
+        The predictions of the model.
+    class_names : list-like
+        A list of class names in the order they are expected in the labels.
+
+    Notes
+    -----
+    The plot is a heatmap using seaborn's heatmap function with the following settings:
+    - x-axis labels are the predicted labels
+    - y-axis labels are the true labels
+    - the color bar is on the right
+    - the title is "Confusion Matrix"
+    - the x-axis label is "Predicted label"
+    - the y-axis label is "True label"
+    """
+    
     cm = confusion_matrix(
         true_labels,
         np.argmax(predictions, axis=1),
