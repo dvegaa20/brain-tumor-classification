c122fc090c4eeb06ede34be8c44fb3b1105e56a9
dvegaa20 <diego.vega.camacho@gmail.com>
2025-04-28 00:22:16 -0600

    refactor(models): update VGG16 and MobileNet architectures to improve layer training and change output activation to softmax

--- src/utils/models.py
+++ src/utils/models.py
@@ -29,28 +29,20 @@ def build_enhanced_cnn(input_shape=IMG_SIZE_GRAY, num_classes=4):
     return model
 
 
-# def enhanced_cnn(input_shape=(150, 150, 3), num_classes=4):
-#     model = models.Sequential()
-#     model.add(layers.Conv2D(10, (3, 3), activation="relu", input_shape=input_shape))
-#     model.add(layers.MaxPooling2D((2, 2)))
-#     model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-#     model.add(layers.MaxPooling2D((2, 2)))
-#     model.add(layers.Flatten())
-#     model.add(layers.Dense(128, activation="relu"))
-#     model.add(layers.Dense(num_classes, activation="sigmoid"))
-#     return model
-
-
 def build_vgg16(input_shape=IMG_SIZE_RGB, num_classes=4):
     base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
-    base_model.trainable = False
+
+    for layer in base_model.layers[:-4]:
+        layer.trainable = False
+    for layer in base_model.layers[-4:]:
+        layer.trainable = True
 
     model = models.Sequential()
 
     model.add(base_model)
     model.add(layers.Flatten())
     model.add(layers.Dense(128, activation="relu"))
-    model.add(layers.Dense(num_classes, activation="sigmoid"))
+    model.add(layers.Dense(num_classes, activation="softmax"))
 
     return model
 
@@ -59,13 +51,18 @@ def build_mobilenet(input_shape=IMG_SIZE_RGB, num_classes=4):
     base_model = MobileNetV2(
         input_shape=input_shape, include_top=False, weights="imagenet"
     )
-    base_model.trainable = False
+
+    for layer in base_model.layers[:-4]:
+        layer.trainable = False
+    for layer in base_model.layers[-4:]:
+        layer.trainable = True
 
     model = models.Sequential()
 
     model.add(base_model)
     model.add(layers.GlobalAveragePooling2D())
     model.add(layers.Dense(128, activation="relu"))
-    model.add(layers.Dense(num_classes, activation="sigmoid"))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation="softmax"))
 
     return model
